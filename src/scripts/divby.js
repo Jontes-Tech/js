00c8317eb705c6ac9071bb4d7b0531f5fb7b9bb2
Jonatan Holmgren <jonatan@jontes.page>
2022-09-10 10:03:57 +0200

    feat: init commit

---
+++ src/scripts/divby.js
@@ -0,0 +1,11 @@
+document.getElementById('content').innerHTML = '<h1>DivBy - A simple tool by Jonte</h1> <p>This tool checks if the first input can be divided by the second one and produce a whole number (an integer)</p><input type="number" id="number" value="14"><br><input type="number" id="divby" value="7"><br><br><button onclick="divby();">Submit</button><br></br>'
+function divby() {
+    numberint = document.getElementById("number").value
+    divbyint = document.getElementById("divby").value
+    if ((numberint % divbyint == 0)) {
+        alert("Your number is dividable by "+divbyint+"!")
+    }
+    else {
+        alert("Your number is not dividable by "+divbyint+".")
+    }
+}
