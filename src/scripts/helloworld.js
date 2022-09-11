09b3430fa7851bc44dcdcbe18573f91ddbc19e1d
Jonatan Holmgren <jonatan@jontes.page>
2022-09-11 17:59:53 +0200

    feat(scripts/helloworld.js): add better hello world

--- src/scripts/helloworld.js
+++ src/scripts/helloworld.js
@@ -1,2 +1,2 @@
 document.getElementById('title').textContent = 'Hello, World'
-document.getElementById('content').innerText = "Hello, World!"
+document.getElementById('content').innerText = "Hello, World! This page does absolutely not need JavaScript, but I made it need it either way."
